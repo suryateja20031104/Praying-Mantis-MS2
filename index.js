e86e83c8101ce1964f5cb8b61a89546f67ba8d2c
suryateja <suryateja1938102074@gmail.com>
2023-09-27 14:47:12 +0000

    solution

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const dbPath = path.join(__dirname, "userDetails.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running at http://localhost:3001/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/userDetailsGet/", async (request, response) => {
+  const getUserQuery = `
+    SELECT
+      *
+    FROM
+      userdetails
+    ;`;
+  const userArray = await db.all(getUserQuery);
+  response.send(userArray);
+});
+
+app.post("/userDetailsAdd/", async (request, response) => {
+  const userDetails = request.body;
+  const { username, email, age, location } = userDetails;
+  const addUser = `
+    INSERT INTO
+      userdetails (username,email,age,location)
+    VALUES
+      (
+        '${username}',
+        '${email}',
+         ${age},
+        '${location}'
+      );`;
+
+  const dbResponse = await db.run(addUser);
+  const userId = dbResponse.lastID;
+  response.send({ userId: userId });
+});
